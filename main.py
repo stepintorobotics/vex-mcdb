07ecb0360e34527c878897cd3657bdd714b7ddd4
seb <sebastien.jensen@stepintorobotics.org.uk>
2024-02-17 23:17:10 +0000

    Fetch events and teams

--- main.py
+++ main.py
@@ -9,10 +9,23 @@ connection = None
 def index():
     return "Hello, world."
 
-@app.route("/test")
-def test():
-    events = re_fetch.fetch_test()
-    return events
+@app.route("/refresh/events")
+def refresh_events():
+    connection = mc_database.connect("indev.db")
+    events = re_fetch.fetch_events(180)
+    mc_database.insert_events(events, connection)
+    events = re_fetch.fetch_events(181)
+    mc_database.insert_events(events, connection)
+    return "OK"
+
+@app.route("/refresh/teams")
+def refresh_teams():
+    connection = mc_database.connect("indev.db")
+    teams = re_fetch.fetch_teams(1)
+    mc_database.insert_teams(teams, connection)
+    teams = re_fetch.fetch_teams(41)
+    mc_database.insert_teams(teams, connection)
+    return "OK"
 
 if __name__ == "__main__":
     connection = mc_database.connect("indev.db")
--- re_fetch.py
+++ re_fetch.py
@@ -1,10 +1,9 @@
 import requests
 import os
 from dotenv import load_dotenv
+import time
 
 url_base = "https://www.robotevents.com/api/v2"
-url_suffix = "/events"
-endpoint = url_base + url_suffix
 
 load_dotenv()
 token = os.environ.get("ACCESS_TOKEN")
@@ -15,16 +14,59 @@ headers = {
     "Authorization": auth_key
 }
 
-def fetch_test():
+
+def fetch_teams(program):
     params = {
-        "region": "United Kingdom",
-        "season": [181]
+        "country": "GB",
+        "program": [program],
+        "registered": True
     }
 
-    response = requests.get(endpoint, headers=headers, params=params)
+    current_page = 1
+    all_data = []
 
-    if response.status_code == 200:
+    url_suffix = "/teams"
+    
+    while True:
+        url_suffix = "/teams?page=" + str(current_page)
+        endpoint = url_base + url_suffix
+
+        response = requests.get(endpoint, headers=headers, params=params)
         data = response.json()
-        return data
-    else:
-        return None
+        all_data.extend(data["data"])
+
+        if current_page == data["meta"]["last_page"]:
+            break
+
+        current_page += 1
+        time.sleep(1)
+
+    return all_data
+
+
+def fetch_events(season):
+    params = {
+        "region": "United Kingdom",
+        "season": [season],
+    }
+
+    current_page = 1
+    all_data = []
+
+    url_suffix = "/events"
+
+    while True:
+        url_suffix = "/events?page=" + str(current_page)
+        endpoint = url_base + url_suffix
+
+        response = requests.get(endpoint, headers=headers, params=params)
+        data = response.json()
+        all_data.extend(data["data"])
+
+        if current_page == data["meta"]["last_page"]:
+            break
+
+        current_page += 1
+        time.sleep(1)
+
+    return all_data
