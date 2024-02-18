571117666e0b5818ca43d272f7541db3e1d44983
seb <sebastien.jensen@stepintorobotics.org.uk>
2024-02-18 11:30:11 +0000

    Multi-purpose fetch function

--- re_fetch.py
+++ re_fetch.py
@@ -15,6 +15,29 @@ headers = {
 }
 
 
+def fetch_data(item, query):
+    params = query
+
+    current_page = 1
+    all_data = []
+    
+    while True:
+        url_suffix = f"/{item}?per_page=250&{str(current_page)}"
+        endpoint = url_base + url_suffix
+
+        response = requests.get(endpoint, headers=headers, params=params)
+        data = response.json()
+        all_data.extend(data["data"])
+
+        if current_page == data["meta"]["last_page"]:
+            break
+
+        current_page += 1
+        time.sleep(1)
+
+    return all_data
+
+
 def fetch_teams(program):
     params = {
         "country": "GB",
