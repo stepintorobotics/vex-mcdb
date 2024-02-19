bfecc16cee9ee3d753dcb1df9e4e31bb084dde9e
seb <sebastien.jensen@stepintorobotics.org.uk>
2024-02-19 10:06:32 +0000

    Combine GET requests for IQ/V5 events, teams

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ def index():
 def refresh_events():
     connection = mc_database.connect("indev.db")
     # Fetch data
-    events = re_fetch.fetch_data("events", {"region": "United Kingdom", "season": [180]})
-    events.extend(re_fetch.fetch_data("events", {"region": "United Kingdom", "season": [181]}))
+    events = re_fetch.fetch_data("events", {"region": "United Kingdom", "season[]": [180, 181]})
     # Insert data into database
     mc_database.insert_events(events, connection)
     connection.close()
@@ -27,8 +26,7 @@ def refresh_events():
 def refresh_teams():
     connection = mc_database.connect("indev.db")
     # Fetch data
-    teams = re_fetch.fetch_data("teams", {"country": "GB", "program": [1], "registered": True})
-    teams.extend(re_fetch.fetch_data("teams", {"country": "GB", "program": [41], "registered": True}))
+    teams = re_fetch.fetch_data("teams", {"country": "GB", "program[]": [1, 41], "registered": True})
     # Insert data into database
     mc_database.insert_teams(teams, connection)
     connection.close()
